5c05aa90e09ab7260dab175b302bf8c0f0ea80b9
JMauC <jmaunc@outlook.com>
2018-05-18 14:35:19 -0300

    Commit inicial contendo os arquivos baixados diretamente do DropBox, sem modificações.

---
+++ src/sistemarestaurante/individuos/Pessoa.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemarestaurante.individuos;
+
+/**
+ *
+ * @author newlo
+ */
+public class Pessoa {
+    private String cpf;
+    private String nome;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private boolean sexoMasculino;
+
+    //Contrutor de classe
+    public Pessoa(String cpf, String nome, String endereco, String email, String telefone, boolean sexoMasculino){
+        setCpf(cpf);
+        setNome(nome);
+        setEndereco(endereco);
+        setEmail(email);
+        setTelefone(telefone);
+        setGenero(sexoMasculino);
+    }
+    public Pessoa(){
+        this(null, null, null, null, null, false);
+    }
+
+    /**
+     * todos os sets tem a função de definir o que esta a seguir ex setName-define o nome
+     * todos os get tema função de pegar o valor do que esta a seguir ex getName- recebe o valor de name
+     */
+    protected void setCpf(String este)
+    {
+        this.cpf=este;
+    }
+    protected String getCpf()
+    {
+        return this.cpf;
+    }
+    protected void setNome(String este)
+    {
+        this.nome=este;
+    }
+    protected String getNome()
+    {
+        return this.nome;
+    }
+    protected void setEndereco(String este)
+    {
+        this.endereco=este;
+    }
+    protected String getEndereco()
+    {
+        return this.endereco;
+    }
+    protected void setTelefone(String este)
+    {
+        this.telefone=este;
+    }
+    protected String getTelefone()
+    {
+        return this.telefone;
+    }
+    protected void setEmail(String este)
+    {
+        this.email=este;
+    }
+    protected String getEmail()
+    {
+        return this.email;
+    }
+    protected void setGenero(boolean este)
+    {
+        this.sexoMasculino=este;
+    }
+    protected boolean getGenero()
+    {
+        return this.sexoMasculino;
+    }
+}
