b158a601845b3b684b61fcbed7741dcdeee19c5c
erikbrito93 <35342966+erikbrito93@users.noreply.github.com>
2018-06-06 15:29:13 -0300

    Add files via upload

--- src/sistemarestaurante/MenuGarcom.java
+++ src/sistemarestaurante/MenuGarcom.java
@@ -11,23 +11,54 @@ public class MenuGarcom {
         Scanner input = new Scanner(System.in);
         int opcao;
 
-        System.out.println("[1] Cadastrar novo pedido.");
+        System.out.println("[1] Recepcionar proximo cliente.");
+        System.out.println("[2] Realizar novo pedido.");
+        System.out.println("[3] Consultar pedidos prontos.");
+        System.out.println("[4] Entregar pedido pronto.");
+        System.out.println("[5] Receber pagamento de pedido.");
+        System.out.println("[0] Sair.");
         System.out.println();
 
         System.out.print("Digite a opção desejada: ");
         opcao = Integer.parseInt(input.nextLine());
 
-        switch(opcao){
-            case 1:
-                realizaPedido(cpfUsuario, 1);
-                break;
-            
-            default:
-                System.out.println("Opcao invalida!");
-        }
+        while(opcao){
+            switch(opcao){
+                case 1:
+                    //Chamada de método
+                    break;
+
+                case 2:
+                    realizaPedido(cpfUsuario, 1);
+                    break;
+                
+                case 3:
+                    //Chamada de método
+                    break;
+                    
+                case 4:
+                    //Chamada de método
+                    break;
+                    
+                case 5:
+                    //Chamada de método
+                    break;
+
+                case 0:
+                    return
+                
+                default:
+                    System.out.println("Opcao invalida!");
+            }
         //input.close();
+        }
     }
 
+    public static void recepcionaCliente() {
+        Scanner input = new Scanner();
+
+        System.out.println("")
+    }
 
     public static void realizaPedido(String cpfGarcom, int codigoMesa) throws SQLException{
         Scanner input = new Scanner(System.in);
@@ -60,5 +91,26 @@ public class MenuGarcom {
 
         //input.close();
         pedido.insereBanco();
+    }
+
+
+    public static void consultaPedidos() {
+        Scanner input = new Scanner();
+
+        System.out.println("")
+    }
+
+
+    public static void entregaPedido(int codPedido) {
+        Scanner input = new Scanner();
+
+        System.out.println("")
+    }
+
+
+    public static void recebePagamento(int codPedido) {
+        Scanner input = new Scanner();
+
+        System.out.println("")
     }
 }
